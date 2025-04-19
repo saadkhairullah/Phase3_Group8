95d676bd5a4c2dd170b36178d0f6df7134e31a14
saadkhairullah <144730913+saadkhairullah@users.noreply.github.com>
2025-04-19 15:24:31 -0500

    finished_app

--- Desktop/DB-Phase3/app.py
+++ Desktop/DB-Phase3/app.py
@@ -22,3 +22,43 @@ def index():
 @app.route('/products', methods =['POST'])
 def add_product():
     data = request.json
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO products (name, price, qunatity) VALUES (%s, %s, %s)",
+                   ( data['name'], data['price'], data['quantity'])) 
+    conn.commit()
+    conn.close()
+    return jsonify({'message': 'Product Added!'}), 201
+
+@app.route('/products', methods=['GET'])
+def get_products():
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM products") 
+    cursor.fetchall()
+    conn.close()
+    return jsonify(products)
+
+@app.route('/products/<int:id>', methods=['PUT'])
+def update_products(id):
+    data = request.json
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("UPDATE products SET name=%s, price=%s, quantity=%s",
+                   ( data['name'], data['price'], data['quantity'], id)) 
+    conn.commit()
+    conn.close()
+    return jsonify({'message': 'Product Updated!'})
+
+@app.route('/products/<int:id>', methods=['DELETE'])
+def delete_products(id):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM products WHERE id=%s", (id,)) 
+    conn.commit()
+    conn.close()
+    return jsonify({'message': 'Product Deleted!'})
+    
+    
+if __name__ == '__main__':
+        app.run(debug=True)
